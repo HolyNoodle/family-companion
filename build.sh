f2558d35f8af673fc069c64fe2e67f8f19df6a3f
HolyNoodle <kbriffaut@gmail.com>
2023-04-27 12:44:58 +0200

    fix: build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-docker build . -f Dockerfile.prod -t holynoodledev/family-companion:0.10
+docker build . -f Dockerfile -t holynoodledev/family-companion:0.11
 
 cat ./password.txt | docker login -u holynoodledev --password-stdin
-docker push holynoodledev/family-companion:0.10
+docker push holynoodledev/family-companion:0.11
