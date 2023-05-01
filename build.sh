e586489ae004bb6045767606ece9b16858540180
HolyNoodle <kbriffaut@gmail.com>
2023-05-01 12:52:49 +0200

    chore: improve docker build

--- build.sh
+++ build.sh
@@ -1,4 +1,19 @@
-docker build . -f Dockerfile -t holynoodledev/family-companion:0.11
+#!/bin/sh
+VERSION=$(cat version)
 
+echo "Building image version: $VERSION"
+docker build . -f Dockerfile.local -t holynoodledev/family-companion:$VERSION
+
+echo "Pushing to docker hub"
 cat ./password.txt | docker login -u holynoodledev --password-stdin
-docker push holynoodledev/family-companion:0.11
+docker push holynoodledev/family-companion:$VERSION
+
+echo "Bump home assistant addon version: $VERSION"
+sed -i "s/version: .*/version: $VERSION/" config.yaml 
+
+echo "Update main Dockerfile image version: $VERSION"
+sed -i "s/FROM holynoodledev\/family-companion:.*/FROM holynoodledev\/family-companion:$VERSION/" Dockerfile 
+
+git add config.yaml Dockerfile
+git commit -m "chore: version bump $VERSION"
+git push origin main
