c4d8697b4a6c81ae5917e6f8d924e2969d81568e
HolyNoodle <kbriffaut@gmail.com>
2023-05-02 18:17:29 +0200

    feat: set up jest coverage

---
+++ libs/config-jest/jest.config.js
@@ -0,0 +1,14 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  collectCoverageFrom: ["src/**/*.ts"],
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
+  },
+};
