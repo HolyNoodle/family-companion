f4a5435d397e7368da6afae3415441e69057de19
HolyNoodle <kbriffaut@gmail.com>
2023-05-02 13:45:10 +0200

    test

--- bump_version.sh
+++ bump_version.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 VERSION=$(cat VERSION)
 
+echo "Bump home assistant addon version: $VERSION"
+sed -i "s/version: .*/version: $VERSION/" config.yaml 
+
 echo "Update main Dockerfile image version: $VERSION"
 sed -i "s/holynoodledev\/family-companion:.*/holynoodledev\/family-companion:$VERSION/" Dockerfile 
 
